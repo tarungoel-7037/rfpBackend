fb6ff97430ec9f79fd9feaeb570f019ee3b70d9c
Tarun Goel <tarun.goel@velsof.com>
2026-03-26 10:49:01 +0530

    Model and DB setup

---
+++ rfp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RfpConfig(AppConfig):
+    name = 'rfp'
---
+++ rfpBackend/__init__.py
@@ -0,0 +1,9 @@
+import pymysql
+
+# Django 6 checks the MySQLdb client version during backend startup.
+# When PyMySQL is used as a drop-in replacement, expose a compatible
+# version tuple/string so the backend can proceed.
+pymysql.version_info = (2, 2, 1, "final", 0)
+pymysql.__version__ = "2.2.1"
+
+pymysql.install_as_MySQLdb()
